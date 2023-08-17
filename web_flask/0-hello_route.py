a6b77c63b0ff0aec4406e046a6a6cc5486bf391c
Vivi-anne <viviannekwamboka98@gmail.com>
2023-08-17 22:01:49 +0000

    application server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,15 @@
 """Starts a Flask web application.
 """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    return render_template("10-hbnb_filters.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=None)
