d416d7261159c1331a2586c2811312b4aba4123a
Vivi-anne <viviannekwamboka98@gmail.com>
2023-08-18 15:06:34 +0000

    updates

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
 """
 
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask
 
+app = Flask("__name__")
 
-@app.route("/", strict_slashes=False)
+@app.route('/')
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return render_template("10-hbnb_filters.html")
+    """Return a given string"""
+    return ("Hello HBNB!")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000)
